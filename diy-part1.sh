af368d2400935a25afff67ade5fd184a29e2d13d
koulas <koulas2046@gmail.com>
2022-11-29 18:42:41 +0800

    Initial commit

---
+++ diy-part1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/Lancenas/Actions-Lean-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# fw876/helloworld
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+# Lienol/openwrt-package
+#sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
